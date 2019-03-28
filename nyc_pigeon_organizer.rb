e2c237491d55c55e4e7250d3473c67ec6b956f97
Joe Tustin <j.tustin@gmail.com>
2019-03-28 00:13:00 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,49 @@
+require 'pry'
+
+
 def nyc_pigeon_organizer(data)
   # write your code here!
+
+  pigeon_list = Hash.new
+
+  name_array = Array.new
+
+
+
+  data.each do |element, element_value|
+    element_value.each do |attribute, attribute_value|
+      attribute_value.each do |names| # shovels over the name and then sets the variable equal to the uniq value
+        name_array << names
+        name_array = name_array.uniq
+
+      end
+    end
+  end
+
+
+  name_array.each do |name|
+    data.each do |element, element_value|
+      element_value.each do |attribute, attribute_value|
+        attribute_value.find do |names|
+          if names == name
+
+
+            pigeon_list[name]= pigeon_list[name].merge.({element=> [attribute]})
+
+
+          #  pigeon_list[name] = {element=> attribute}.merge.pigeon_list[name]
+
+            binding.pry
+          end
+        end
+      end
+    end
+  end
+
+
+
+
+
+
+
 end
