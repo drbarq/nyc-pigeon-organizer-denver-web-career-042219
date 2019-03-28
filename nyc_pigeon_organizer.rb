d1316278989293620df15e468055df6c1b1fec7a
Joe Tustin <j.tustin@gmail.com>
2019-03-28 01:33:20 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,6 +3,50 @@ require 'pry'
 
 #first get each unique name in name_array
 #second build new hash for each name
+=begin
+def nyc_pigeon_organizer(data)
+
+  pigeon_list = Hash.new
+  name_array = Array.new
+
+  data.each do |element, element_value|
+    element_value.each do |attribute, attribute_value|
+      attribute_value.each do |names| # shovels over the name and then sets the variable equal to the uniq value
+        name_array << names
+        name_array = name_array.uniq
+      end
+    end
+  end
+
+
+
+# this works and builds the name and info but then ends, doesnt do the whole array of names
+
+  name_array.each do |name|
+      binding.pry
+    data.each do |element, element_value|
+      element_value.each do |attribute, attribute_value|
+        attribute_value.each do |names|
+          if names == name
+            if pigeon_list.length > 0 && pigeon_list[name].has_key?(element)
+              pigeon_list[name][element] << attribute.to_s
+            elsif pigeon_list.has_key?(name)
+              pigeon_list[name] = pigeon_list[name].merge({element=> [attribute.to_s]})
+            else
+              pigeon_list[name]= {element=> [attribute.to_s]}
+            end
+          end
+        end
+      end
+    end
+    return pigeon_list
+  end
+
+
+
+end
+=end 
+
 
 def nyc_pigeon_organizer(data)
 
