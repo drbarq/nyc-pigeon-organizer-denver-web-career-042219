85e45a87db999b3826a2ec19c865b2cbb2df0c04
Joe Tustin <j.tustin@gmail.com>
2019-03-28 01:10:01 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -22,7 +22,7 @@ def nyc_pigeon_organizer(data)
 
   name_array.each do |name|
     data.each do |element, element_value|
-      element_value.map do |attribute, attribute_value|
+      element_value.each do |attribute, attribute_value|
         attribute_value.find do |names|
           if names == name
             binding.pry
