706a7aeaa22881eb784f7e7d554bc9174b912828
Joe Tustin <j.tustin@gmail.com>
2019-03-28 01:46:19 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -75,7 +75,7 @@ def nyc_pigeon_organizer(data)
 
 
 # this works and builds the name and info but then ends, doesnt do the whole array of names
-
+=begin
   name_array.collect do |name|
     data.each do |element, element_value|
       element_value.each do |attribute, attribute_value|
@@ -96,5 +96,6 @@ def nyc_pigeon_organizer(data)
    binding.pry
   end
 binding.pry
+=end
 
 end
