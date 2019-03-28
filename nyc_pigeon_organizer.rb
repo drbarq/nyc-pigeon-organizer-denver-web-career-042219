a0d00c72bec275ae8c82cbfd0a7705c143c2f3bf
Joe Tustin <j.tustin@gmail.com>
2019-03-28 00:47:43 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -34,8 +34,8 @@ def nyc_pigeon_organizer(data)
             if pigeon_list.has_key?(name)
               pigeon_list[name] = pigeon_list[name].merge({element=> [attribute]})
               binding.pry
-            elsif pigeon_list.has_key?(pigeon_list[name])
-              pigeon_list[name][attribute] << attribute
+            elsif pigeon_list[name].has_key?(element)
+              pigeon_list[name][element] << attribute
               binding.pry
             else
               pigeon_list[name]= {element=> [attribute]}
